de042fb892e346ccd6e20d5c35c62677aefefb6a
steppenwolf <margolin.dm@gmail.com>
2021-03-22 03:11:23 +0200

    pushing to master using bush file

--- socialnetwork/blog/views.py
+++ socialnetwork/blog/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from .models import Post, Likes
+from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -7,10 +7,4 @@ class PostList(generic.ListView):
 
 class PostDetail(generic.DetailView):
     model = Post
-    try:
-        likes = Likes.objects.get(id_post=Post.id_post,like_status=1).blog_likes.count()
-        unlikes = Likes.objects.get(id_post=Post.id_post,like_status=2).blog_likes.count()
-    except:
-        likes = 0
-        unlikes = 0
     template_name = 'post_detail.html'
