83502f267d969761f54aae812ed83d23d21a67ce
excelenz <margolin@visart.biz>
2021-03-24 15:38:39 +0200

    asdAS'

--- socialnetwork/bot/views.py
+++ socialnetwork/bot/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import requests
+import random
+import json
+import urllib
+import re
 
+# we wanna call to bot by get URL not from console.
 
 class Bot(APIView):
+
+    urlRegister = "http://127.0.0.1:5000/api/token/register/"
+
     def get(self, request):
-        content = {'message': 'Ok'}
+
+        content = {'message': self.signup()}
         return Response(content)
+
+    def signup(self):
+        payload = {'email': str(random.randrange(100, 1000))+'asfasf@gmail.com', 'username': str(random.randrange(100000, 1000000)) +'user','password': str(random.randrange(100000, 1000000))+'!z34'}
+        r = requests.post(self.urlRegister, data=payload)
+        return r
